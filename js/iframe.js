563744070a68c342cf2faa07c8927fa1342faeb3
Amaury Franssen <amauryfranssen@gmail.com>
2025-04-11 02:32:50 +0200

    feat: modify js to match triggering button video autoplay

--- js/iframe.js
+++ js/iframe.js
@@ -16,5 +16,24 @@ function playVideo() {
     }
   }
   
+  /* Pour les utilisateur pour qui la vidéo ne peux être lu, il y a cette alternative : la transcription */
+  function toggleTranscript() {
+  const transcriptContent = document.getElementById('transcript-content');
+  const button = document.querySelector('.transcript-section button');
+  const isExpanded = button.getAttribute('aria-expanded') === 'true';
 
+  // Toggle the visibility and aria-hidden attribute
+  transcriptContent.hidden = !isExpanded;
+  transcriptContent.setAttribute('aria-hidden', (!isExpanded).toString());
+
+  // Toggle the aria-expanded attribute
+  button.setAttribute('aria-expanded', (!isExpanded).toString());
+
+  // Update the button text
+  if (isExpanded) {
+    button.textContent = 'Fermer la transcription';
+  } else {
+    button.textContent = 'Afficher la transcription';
+  }
+}
 
